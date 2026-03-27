aae6971f7dc12a1e00e37fea0da600b8f0403bdc
FunniNam3 <nguyen.ev03@gmail.com>
2026-03-27 15:54:26 -0500

    Add title

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <title>B4G Checkin</title>
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
